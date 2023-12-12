515a8c82d3107f4274a6f1bf5517934c7e1a8cff
Guy Korland <gkorland@gmail.com>
2023-12-12 20:15:38 +0200

    Update drivers/driver.py
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- drivers/driver.py
+++ drivers/driver.py
@@ -5,13 +5,28 @@ class Driver(ABC):
 
     @abstractmethod
     def get_graph_data(self):
+        """
+        Abstract method to get graph data.
+        """
         pass
 
     @abstractmethod
-    def get_graph_history(self, skip, per_page):
+    def get_graph_history(self, skip: int, per_page: int) -> Any:
+        """
+        Abstract method to get graph history.
+        
+        :param skip: The number of items to skip.
+        :param per_page: The number of items per page.
+        :return: The graph history data.
+        """
         pass
 
     @abstractmethod
-    def get_response_data(self, response_data):
+    def get_response_data(self, response_data: Any) -> Any:
+        """
+        Abstract method to process response data.
+        
+        :param response_data: The response data to process.
+        :return: The processed response data.
+        """
         pass
-    
